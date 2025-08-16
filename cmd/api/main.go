991af9006b97047fd6b20263182eaa3da13c801e
michu <misiek1027@o2.pl>
2025-08-16 15:59:37 +0200

    chore: remove comments

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,10 +25,8 @@ func gracefulShutdown(apiServer *http.Server, done chan bool) {
 	<-ctx.Done()
 
 	log.Println("shutting down gracefully, press Ctrl+C again to force")
-	stop() // Allow Ctrl+C to force shutdown
+	stop()
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := apiServer.Shutdown(ctx); err != nil {
